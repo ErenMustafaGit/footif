00d6f583e7d6f006b7cb5602d894b5a7aca8bbf6
“Gabinrun” <gabgarrot@gmail.com>
2024-01-19 18:10:54 +0100

    Modif card (ajout des photos statiques)

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,45 +3,57 @@ import { Heading, Image, Stack, Text } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 export interface CardProps {
-    icon?: string;
-    title?: string;
-    subtitle?: string;
-    wikiId?: string;
-    type?: string;
-    width?: string;
-};
+  icon?: string;
+  title?: string;
+  subtitle?: string;
+  wikiId?: string;
+  type?: string;
+  width?: string;
+}
 
+export const Card = ({
+  icon,
+  title,
+  subtitle,
+  wikiId,
+  type,
+  width,
+}: CardProps) => {
+  const navigator = useNavigate();
 
-
-export const Card = ({ icon, title, subtitle, wikiId, type, width }: CardProps) => {
-    const navigator = useNavigate();
-
-    const handleClick = () => {
-        console.log(title);
-        switch (type) {
-            case "player":
-                navigator(`/player/${wikiId}`);
-                break;
-            case "team":
-                navigator(`/team/${wikiId}`);
-                break;
-            case "tournament":
-                navigator(`/tournament/${wikiId}`);
-                break;
-            default:
-                break;
-        }
+  const handleClick = () => {
+    console.log(title);
+    switch (type) {
+      case "player":
+        navigator(`/player/${wikiId}`);
+        break;
+      case "team":
+        navigator(`/team/${wikiId}`);
+        break;
+      case "tournament":
+        navigator(`/tournament/${wikiId}`);
+        break;
+      default:
+        break;
     }
-    
-    return (
-        <CardBase variant={"outline"} width={width} onClick={()=>handleClick()}>
-            <Image src="" />
-            <Stack>
-                <CardBody>
-                    <Heading size={"md"}>{title}</Heading>
-                    <Text>{subtitle}</Text>
-                </CardBody>
-            </Stack>
-        </CardBase>
-    );
+  };
+
+  return (
+    <CardBase
+      size="100px"
+      direction={{ base: "column", sm: "row" }}
+      variant={"outline"}
+      width={width}
+      onClick={() => handleClick()}
+    >
+      <Image objectFit="cover" src={icon} maxW={{ base: '100%', sm: '100px' }} />
+
+      <Stack padding ="2">
+        <CardBody>
+          <Heading size={"md"}>{title}</Heading>
+          <Text py="2">{subtitle}</Text>
+        </CardBody>
+      </Stack>
+    </CardBase>
+  );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,84 +1,121 @@
-import { Box, Spinner } from "@chakra-ui/react"
-import { Card, Searchbar } from "../../components"
-import { useEffect, useState } from "react"
-import { useFetchSearch } from "../../queries"
+import { Box, Spinner } from "@chakra-ui/react";
+import { Card, Searchbar } from "../../components";
+import { useEffect, useState } from "react";
+import { useFetchSearch } from "../../queries";
 
 export const Home = () => {
-    const [search, setSearch] = useState<string>("");
-    const { isLoading, data, error } = useFetchSearch(search);
+  const [search, setSearch] = useState<string>("");
+  const { isLoading, data, error } = useFetchSearch(search);
 
-    const handleSubmitSearch = (val: string) => {
-        setSearch(val);
-    };
+  const handleSubmitSearch = (val: string) => {
+    setSearch(val);
+  };
 
-    useEffect(() => {
-        console.log(search);
-    }, [search]);
+  useEffect(() => {
+    console.log(search);
+  }, [search]);
 
-    useEffect(() => {
-        console.log(data);
-    }, [data]);
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
-    return (
-        <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            gap="32px"
-            padding="32px"
-            width="100%"
-        >
-            <Searchbar
-                onSubmit={handleSubmitSearch}
-                placeholder="Recherchez un joueur, un club, ..."
-                width="60%"
-            />
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      gap="32px"
+      padding="32px"
+      width="100%"
+    >
+      <Searchbar
+        onSubmit={handleSubmitSearch}
+        placeholder="Recherchez un joueur, un club, ..."
+        width="60%"
+      />
 
-            <Box
-                display="flex"
-                flexDirection="column"
-                alignItems="center"
-                gap="16px"
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        gap="16px"
+        width="100%"
+      >
+        {data ? (
+          <>
+            {data?.results?.bindings?.map((item: any) => (
+              <Card
+                title={
+                  item.playerName?.value ??
+                  item.clubName?.value ??
+                  item.ligueName?.value ??
+                  "Unsupported type"
+                }
+                subtitle={
+                  item.playerName?.value
+                    ? "Joueur"
+                    : item.clubName?.value
+                    ? "Club"
+                    : item.ligueName?.value
+                    ? "Championnat"
+                    : "Unsupported type"
+                }
+                type={
+                  item.playedID?.value
+                    ? "player"
+                    : item.clubID?.value
+                    ? "team"
+                    : item.ligueID?.value
+                    ? "ligue"
+                    : ""
+                }
+                wikiId={
+                  item.playedID?.value ??
+                  item.clubID?.value ??
+                  item.ligueID?.value ??
+                  "-1"
+                }
                 width="100%"
-            >
-                {data ? (
-                    <>
-                        {data?.results?.bindings?.map((item: any) => (
-                             <Card
-                                title={item.playerName?.value ?? (item.clubName?.value ?? (item.ligueName?.value ?? "Unsupported type"))}
-                                subtitle={item.playerName?.value ? "Joueur" : (item.clubName?.value ? "Club" : (item.ligueName?.value ? "Championnat" : "Unsupported type"))}
-                                type={item.playedID?.value ? "player" : (item.clubID?.value ? "team" : (item.ligueID?.value ? "ligue" : ""))}
-                                wikiId={item.playedID?.value ?? (item.clubID?.value ?? (item.ligueID?.value ?? "-1"))}
-                                width="100%"
-                            />
-                        ))}
-                    </>
-                ) : (
-                    <>
-                        {isLoading ? (
-                            <Spinner />)
-                        : (
-                            <>
-                                <Card
-                                    title="Lionel Messi"
-                                    subtitle="Joueur"
-                                    type="player"
-                                    wikiId="2150841"
-                                    width="100%"
-                                />
+              />
+            ))}
+          </>
+        ) : (
+          <>
+            {isLoading ? (
+              <Spinner />
+            ) : (
+              <>
+                <Card
+                  icon="messi.jpg"
+                  title="Lionel Messi"
+                  subtitle="Joueur"
+                  type="player"
+                  wikiId="2150841"
+                  width="100%"
+                />
 
-                                <Card
-                                    title="FC Barcelone"
-                                    subtitle="Club"
-                                    type="team"
-                                    wikiId="68187"
-                                    width="100%"
-                                />
-                            </>
-                        )}
-                    </>
-                )}
-            </Box>
-        </Box>
-    )
-}
+                <Card
+                  icon="barca.png"
+                  title="FC Barcelone"
+                  subtitle="Club"
+                  type="team"
+                  wikiId="68187"
+                  width="100%"
+                />
+
+                <Card
+                  icon="ronaldo.jpeg"
+                  title=" Cristiano Ronaldo"
+                  subtitle="Joueur"
+                  type="team"
+                  wikiId="68187"
+                  width="100%"
+                />
+              </>
+            )}
+          </>
+        )}
+      </Box>
+    </Box>
+  );
+};
